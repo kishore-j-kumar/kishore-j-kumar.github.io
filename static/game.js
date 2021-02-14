755f9289177aa6aa0a3557971d5519178e040835
kishore-j-kumar <kishore.kumar@berkeley.edu>
2021-02-14 12:05:31 -0800

    qol changes

--- static/game.js
+++ static/game.js
@@ -10,6 +10,12 @@ var ins = {
   in3: []
 }
 
+var counter = {
+	set1: 20,
+	set2: 20,
+	set3: 20
+}
+
 var players = ["Pl1", "Pl2"]
 var colors = ["blue", "red"]
 var turn = 1
@@ -23,7 +29,7 @@ document.getElementById("done").onclick = function() {
 	turn = 1 - turn
 	document.getElementById("turn").textContent = players[turn] + "\'s turn to draw/ask!";
 	document.getElementById("turn").style.color = colors[turn]
-	if (ins.in3.length === 0) {
+	if (counter.set3 === 0) {
 		alert("Congratulations! You have finished all of the questions!");
 		document.getElementById("done").disabled = true;
 		document.getElementById("draw").disabled = true;
@@ -35,27 +41,30 @@ document.getElementById("draw").onclick = function() {
 	document.getElementById("draw").style.background = "gray"; 
 	document.getElementById("done").style.background = "#0275d8"; 
 	document.getElementById("done").disabled = false;
-	document.getElementById("turn").textContent = players[(1-turn)] + " answers now!";
+	document.getElementById("turn").textContent = players[(1-turn)] + " answers for " + players[turn] + " now!";
 	document.getElementById("turn").style.color = colors[(1-turn)]
-	if (ins.in1.length !== 0) {
+	if (counter.set1 !== 0) {
 		document.getElementById("level").textContent = "Level 1 - Perception";
 		var index = ins.in1[Math.floor(Math.random() * ins.in1.length)];
 		document.getElementById("card_text").textContent = strings.level1[index];
 		ins.in1 = ins.in1.filter(item => item !== index);
+		counter.set1 -= 1;
 		return;
 	}
-	if (ins.in2.length !== 0) {
+	if (counter.set2 !== 0) {
 		document.getElementById("level").textContent = "Level 2 - Connection";
 		var index = ins.in2[Math.floor(Math.random() * ins.in2.length)];
 		document.getElementById("card_text").textContent = strings.level2[index];
 		ins.in2 = ins.in2.filter(item => item !== index);
+		counter.set2 -= 1;
 		return;
 	}
-	if (ins.in3.length !== 0) {
+	if (counter.set3 !== 0) {
 		document.getElementById("level").textContent = "Level 3 - Reflection";
 		var index = ins.in3[Math.floor(Math.random() * ins.in3.length)];
 		document.getElementById("card_text").textContent = strings.level3[index];
 		ins.in3 = ins.in3.filter(item => item !== index);
+		counter.set3 -= 1;
 		return;
 	}
 	alert("Congratulations! You have finished all of the questions!");
